111303cf59c2980408b1d0adfb2462b26ee6b0d5
Gerijacki <gerardloriz05@gmail.com>
2024-11-09 02:02:49 +0100

    Easter Egg

--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -1,4 +1,6 @@
 const reportWebVitals = onPerfEntry => {
+  console.log("%cHey there, hacker! 👀", "color: #00ff00; font-size: 16px; font-weight: bold;");
+  console.log("%cflag{c0ns0le_3ast3r_egg;)}", "color: #9c2200;");
   if (onPerfEntry && onPerfEntry instanceof Function) {
     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
       getCLS(onPerfEntry);
