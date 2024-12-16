9da97585da3f23818cd1613f7bf9df4aa8e95b54
Gerijacki <gerardloriz05@gmail.com>
2024-12-16 17:00:44 +0100

    Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
                 <p className="small mb-0 text-light">
                     &copy;
                     <script>document.write(new Date().getFullYear())</script> Created With <i
-                        className="ti-heart text-danger"></i> By <a href="https://gerijacki.github.io" target="_blank"><span
+                        className="ti-heart text-danger"></i> By <a href="https://github.com/gerijacki" target="_blank"><span
                             className="text-danger" title="Bootstrap 4 Themes and Dashboards">Gerijacki</span></a>
                 </p>
             </div>
